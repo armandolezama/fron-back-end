a090a239a6e4b6be9238f067dda60fd693bbdbd7
armandolezama <armando.lezama.psic@gmail.com>
2023-01-22 23:21:10 -0600

    fix(js): updated app class and implementation

---
+++ contactos/ContactBookApp.js
@@ -0,0 +1,152 @@
+import { ContactTemplateController } from "./ContactTemplateController.js";
+import { ContactAPI } from "./ContactAPI.js";
+
+export class ContactBookApp {
+  constructor(){
+
+    this.url = 'http://localhost:8000';
+    
+    this.templateController = new ContactTemplateController();
+    
+    this.contactApi = new ContactAPI(this.url);
+
+    console.log(this.templateController)
+
+  }
+
+  async getContacts(){
+    
+    await this.contactApi.getAllContacts();
+
+    this.printIcons(this.contactApi.response);
+
+  }
+
+  async sendData(){
+
+    await this.contactApi.createContact(this.templateController.getNewContactFormData());
+
+    this.printIcons(this.contactApi.response);
+
+    this.templateController.disableModal();
+
+    this.templateController.clearNewContactForm();
+
+  };
+
+  async submitHandler(event) {
+    
+    event.preventDefault()
+    
+    const { name, email, ubication } = this.templateController.getEditContactFormData();
+
+    if(
+        name == "" ||
+        email == "" ||
+        ubication == ""
+    ){
+        alert('Por favor, llene los campos antes de enviar la actualización')
+    } else {
+
+        const id = this.templateController.getActiveCardId();
+
+        await this.contactApi.editContact({id, name, email, ubication });
+
+        this.printIcons(this.contactApi.response);
+
+        this.templateController.updateCardClicked(id);
+
+        this.templateController.disableModal();
+    
+        this.templateController.clearNewContactForm();
+
+    }
+  }
+
+  async cardEventHandler(){
+
+    const id = this.templateController.getActiveCardId();
+
+    await this.contactApi.getSingleContact(id);
+    
+    this.templateController.updateCardView(this.contactApi.response);
+
+  }
+
+  printIcons(list){
+    
+    this.templateController.printIcons(list);
+    
+    this.templateController.setCardCustomEventHandler(this.cardEventHandler.bind(this));
+
+  }
+
+  async editUser(){
+
+    if(this.templateController.getActiveCardId() == ""){
+    
+      alert('Por favor, seleccione un contacto');
+    
+    } else {
+    
+      const id = this.templateController.getActiveCardId();
+    
+      await this.contactApi.getSingleContact(id);
+    
+      this.templateController.enableEditFormModal();
+    
+      this.templateController.setEditContactForm(this.contactApi.response);
+    
+    }
+  }
+
+  deleteUser(){
+    
+    if(this.templateController.getActiveCardId() == ""){
+    
+      alert('Por favor, seleccione un contacto')
+    
+    } else {
+    
+      this.templateController.enableDeleteFormModal();
+    
+    }
+
+  }
+
+  async deleteContact(){
+
+    const id = this.templateController.getActiveCardId();
+    
+    await this.contactApi.deleteContact(id)
+    
+    this.printIcons(this.contactApi.response);
+
+    this.templateController.cardView.innerHTML = "";
+
+    this.templateController.disableModal();
+
+  };
+
+  cancelDeleteContact(){
+    this.templateController.disableModal();
+};
+
+  start(){
+
+    this.templateController.submitEditButton.addEventListener('click', this.submitHandler.bind(this));
+
+    this.templateController.modalSendDataButton.addEventListener('click', this.sendData.bind(this));
+    
+    this.templateController.editUserOption.addEventListener('click', this.editUser.bind(this));
+    
+    this.templateController.deleteUserOption.addEventListener('click', this.deleteUser.bind(this));
+    
+    this.templateController.modalDeleteContactButton.addEventListener('click', this.deleteContact.bind(this));
+
+    this.templateController.modalCancelDeleteContactButton.addEventListener('click', this.cancelDeleteContact.bind(this));
+
+    this.getContacts();
+    
+  }
+ }
--- contactos/ContactTemplateController.js
+++ contactos/ContactTemplateController.js
@@ -1,38 +1,65 @@
 export class ContactTemplateController {
   constructor(){
+    
     this.cardView = document.getElementById('cards-list');
+    
     this.cardsIcon = document.getElementById('cards-icon');
+    
     this.iconsForm = document.getElementById('icons-form');
+    
     this.submitButton = document.getElementById('submit-button');
+    
     this.submitEditButton = document.getElementById('submit-edit-button');
+    
     this.newData = document.getElementById('new-data');
+    
     this.editedData = document.getElementById('edit-data');
+    
     this.modalActive = document.querySelector('.modal');
+    
     this.modalEdit = document.querySelector('.modal-edit');
+    
     this.modalPost = document.querySelector('.modal-post-user');
+    
     this.modalDelete = document.querySelector('.modal-delete');
+    
     this.modalSendDataButton = document.querySelector('#modal-send-data-button');
+    
     this.modalCancelSendDataButton = document.querySelector('#modal-cancel-send-data-button');
+    
     this.editUserOption = document.querySelector('#edit-user-option');
+    
     this.deleteUserOption = document.querySelector('#delete-user-option');
+    
     this.modalDeleteContactButton = document.querySelector('#modal-delete-contact-button');
+    
     this.modalCancelDeleteContactButton = document.querySelector('#modal-cancel-delete-contact-button');
+    
     this.loadListeners();
   };
 
   loadListeners(){
+
     this.submitButton.addEventListener('click', event => {
+      
       event.preventDefault()
   
       if(
-          this.newData.name.value == "" ||
-          this.newData.email.value == "" ||
-          this.newData.location.value == ""
+        
+        this.newData.name.value == "" ||
+        
+        this.newData.email.value == "" ||
+        
+        this.newData.location.value == ""
+
       ){
           alert('Por favor, llene todos los campos antes de enviar')
       } else {
-          this.modalActive.classList.add('active');
-          this.modalPost.classList.add('active');
+          
+        this.modalActive.classList.add('active');
+        
+        this.modalPost.classList.add('active');
+
       };
   
     });
@@ -42,60 +69,91 @@ export class ContactTemplateController {
     });
 
     this.modalActive.addEventListener('click', event => {
+      
       if(event.target === event.currentTarget){
-          this.modalActive.classList.remove('active');
-          this.modalEdit.classList.remove('active');
-          this.modalDelete.classList.remove('active');
-          this.modalPost.classList.remove('active');
+      
+        this.modalActive.classList.remove('active');
+      
+        this.modalEdit.classList.remove('active');
+      
+        this.modalDelete.classList.remove('active');
+      
+        this.modalPost.classList.remove('active');
+      
       };
   });
   }
 
   getNewContactFormData(){
+
     const name = this.newData.name.value;
+    
     const email = this.newData.email.value;
+    
     const ubication = this.newData.location.value;
 
     return { name, email, ubication };
+
   }
 
   getEditContactFormData(){
+    
     const name = this.editedData.name.value;
+    
     const email = this.editedData.email.value;
+    
     const ubication = this.editedData.location.value;
 
     return { name, email, ubication };
   }
 
   disableModal(){
+    
     this.modalActive.classList.remove('active');
+    
     this.modalEdit.classList.remove('active');
+    
     this.modalDelete.classList.remove('active');
+    
     this.modalPost.classList.remove('active');
+
   }
 
   clearNewContactForm(){
+    
     this.newData.name.value = "";
+    
     this.newData.email.value = "";
+    
     this.newData.location.value = "";
   }
 
   setEditContactForm({ name = '', email = '', ubication = '' }){
+    
     this.editedData.name.value = name;
+    
     this.editedData.email.value = email;
+    
     this.editedData.location.value = ubication;
   }
 
   updateCardClicked(id = 0 || ''){
+    
     const cards = document.querySelectorAll(".icons-input");
+    
     for(let i = 0; i < cards.length; i++){
-        if(cards[i].value == id){
-            cards[i].click();
-        };
+    
+      if(cards[i].value == id){
+    
+        cards[i].click();
+    
+      };
+
     };
   }
 
   updateCardView(user){
+    
     this.cardView.innerHTML = `
       <div class='card'> 
         <div class="card-inner">
@@ -108,11 +166,15 @@ export class ContactTemplateController {
         </div>
       </div>
     `;
+
   }
 
   printIcons(list){
+    
     this.iconsForm.innerHTML = '';
+    
     for(let user of list.contactList){
+    
       this.iconsForm.innerHTML += 
        `
         <label class='icon' for="icon${user.id}">
@@ -120,23 +182,37 @@ export class ContactTemplateController {
         </label>
         <input class="icons-input" type="radio" name="formIcons" id="icon${user.id}" value="${user.id}">
        `;
-   }
+    }
   }
   
   setCardCustomEventHandler(handler){
+    
     const cards = document.querySelectorAll(".icons-input");
+    
     for(const card of cards){
+    
       card.addEventListener("change", handler);
+    
     };
   }
 
   enableEditFormModal(){
+    
     this.modalActive.classList.add('active');
+    
     this.modalEdit.classList.add('active');
   }
 
   enableDeleteFormModal(){
+    
     this.modalActive.classList.add('active');
+    
     this.modalDelete.classList.add('active');
+    
+  }
+
+  getActiveCardId(){
+    this.iconsForm = document.getElementById('icons-form');
+    return this.iconsForm.formIcons.value;
   }
 }
